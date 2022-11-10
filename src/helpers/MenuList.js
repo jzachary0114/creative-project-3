b776554f01ca429f8e6d7e4ab31ccbb090dd54c0
Zachary Johnson <jzachary0114@gmail.com>
2022-11-10 17:28:11 +0000

    Add existing project files prior to the push to GitHub.

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,40 @@
+import Utah from "../assets/utahBurger.jpg"
+import NewMex from "../assets/newMexicoBurger.jpg"
+import MushSwiss from "../assets/mushroomSwiss.jpg"
+import Guac from "../assets/guacamoleBurger.jpg"
+import Bacon from "../assets/baconBurger.jpg"
+import southWest from "../assets/southWestBurger.jpg"
+
+
+export const MenuList = [
+    {
+        name: "Utah Burger",
+        image: Utah,
+        price: 8.99,
+    },
+    {
+        name: "New Mexico Burger",
+        image: NewMex,
+        price: 9.99,
+    },
+    {
+        name: "Mushroom Swiss Burger",
+        image: MushSwiss,
+        price: 9.99,
+    },
+    {
+        name: "Guacamole Burger",
+        image: Guac,
+        price: 10.99,
+    },
+    {
+        name: "Bacon Burger",
+        image: Bacon,
+        price: 11.99,
+    },
+    {
+        name: "South West Burger",
+        image: southWest,
+        price: 11.99,
+    },
+]
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import BannerImage from '../assets/burgerhome.avif'
+import '../styles/Home.css'
+
+function Home() {
+  return (
+    <div className='home' style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className='headerContainer' >
+        <h1>ZACH'S BURGERS</h1>
+        <p>BEST BURGERS IN TOWN</p>
+        <Link to='/menu'>
+        <button> ORDER NOW </button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
